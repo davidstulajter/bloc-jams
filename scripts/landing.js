01bd7f30727d93231105f17c0eeae6d6af69d264
David Stulajter <davidstulajter@gmail.com>
2016-03-26 16:35:26 -0500

    cp29 jquery landing page -bug fixed, page working.

--- scripts/landing.js
+++ scripts/landing.js
@@ -1,6 +1,5 @@
 
 var animatePoints = function() {   
-    
     var revealPoint = function() {
         $(this).css({
             opacity: 1,
@@ -10,20 +9,16 @@ var animatePoints = function() {
  
     $.each($('.point'), revealPoint);
 };
-
-$(window).load = function() {
+$(window).load(function() {
     if ($(window).height() > 950) {
-        animatePoints();
-    }
-    // Above, we automatically animate the points on tall screens when scrolling is disabled
+         animatePoints();
+     }
     var scrollDistance = $('.selling-points').offset().top - $(window).height() + 200;
     $(window).scroll(function(event) {
-        // console.log("Current offset from the top is " + sellingPoints.getBoundingClientRect().top + " pixels");
         if ($(window).scrollTop() >= scrollDistance) {
-            animatePoints();
+             animatePoints();
         } 
     });
 });
-
 
 
